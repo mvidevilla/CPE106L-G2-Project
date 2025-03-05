d00ecf8559aac8fb4cc81117569e805a28763e4e
mvidevilla <mvidevilla@mymail.mapua.edu.ph>
2025-03-05 15:10:17 +0800

    Update CRUD.py

--- ProjectSprint4/CRUD.py
+++ ProjectSprint4/CRUD.py
@@ -245,3 +245,14 @@ def center_window(window, width=300, height=200):
     y = (screen_height // 2) - (height // 2)
     
     window.geometry(f"{width}x{height}+{x}+{y}")
+
+def center_window(window, width=300, height=200):
+    """Centers a given Tkinter window on the screen."""
+    window.update_idletasks()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    
+    window.geometry(f"{width}x{height}+{x}+{y}")
